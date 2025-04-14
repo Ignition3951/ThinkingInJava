a0c2ece46c6cd7c0df24772ae31c5171b2676cef
Utkarsh <ignition3951@gmail.com>
2025-04-14 13:06:50 +0530

    Implement check in ex6
    Check will be to return same string if compression is not possible

--- src/com/utk/ctci/chp1/Example6.java
+++ src/com/utk/ctci/chp1/Example6.java
@@ -3,22 +3,25 @@ package com.utk.ctci.chp1;
 public class Example6 {
 
 	public static void main(String[] args) {
-		String str="aabcccccaaa";
+		String str="abcdee"; //aabcccccaaa abcdee
 		System.out.println(compressString(str));
 	}
 	
 	public static StringBuilder compressString(String str) {
-		int count=1;
+		int count=1,compressedStringLen=0;
 		StringBuilder compressedString = new StringBuilder();
 		for(int i=0;i<str.length();i++) {
 			if((i+1)>=str.length() || str.charAt(i+1)!=str.charAt(i)) {
 				String tmp = ""+str.charAt(i)+count;
 				compressedString=compressedString.append(tmp);
 				count=1;
+				compressedStringLen++;
 			}else {
 				count++;
 			}
 		}
+		if(compressedStringLen==str.length())
+			return new StringBuilder(str);
 		return compressedString;
 	}
 
