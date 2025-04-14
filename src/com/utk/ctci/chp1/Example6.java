cbd6c86db3031e0c1a6640783656c15434de6612
Utkarsh <ignition3951@gmail.com>
2025-04-14 13:17:47 +0530

    Implement efficient way to implement the check

--- src/com/utk/ctci/chp1/Example6.java
+++ src/com/utk/ctci/chp1/Example6.java
@@ -3,26 +3,23 @@ package com.utk.ctci.chp1;
 public class Example6 {
 
 	public static void main(String[] args) {
-		String str="abcdee"; //aabcccccaaa abcdee
+		String str="abcdeeeeeeeeeee"; //aabcccccaaa abcdee
 		System.out.println(compressString(str));
 	}
 	
 	public static StringBuilder compressString(String str) {
-		int count=1,compressedStringLen=0;
+		int count=1;
 		StringBuilder compressedString = new StringBuilder();
 		for(int i=0;i<str.length();i++) {
 			if((i+1)>=str.length() || str.charAt(i+1)!=str.charAt(i)) {
 				String tmp = ""+str.charAt(i)+count;
 				compressedString=compressedString.append(tmp);
 				count=1;
-				compressedStringLen++;
 			}else {
 				count++;
 			}
 		}
-		if(compressedStringLen==str.length())
-			return new StringBuilder(str);
-		return compressedString;
+		return compressedString.length()<str.length() ? compressedString : new StringBuilder(str);
 	}
 
 }
