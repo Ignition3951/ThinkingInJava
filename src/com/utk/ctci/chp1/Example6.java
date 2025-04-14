ce6b8b08b3036bd6dad94e8ac2e1e1cad5dec55b
Utkarsh <ignition3951@gmail.com>
2025-04-14 12:28:58 +0530

    IMplement compressed String

---
+++ src/com/utk/ctci/chp1/Example6.java
@@ -0,0 +1,25 @@
+package com.utk.ctci.chp1;
+
+public class Example6 {
+
+	public static void main(String[] args) {
+		String str="aabcccccaaa";
+		System.out.println(compressString(str));
+	}
+	
+	public static StringBuilder compressString(String str) {
+		int count=1;
+		StringBuilder compressedString = new StringBuilder();
+		for(int i=0;i<str.length();i++) {
+			if((i+1)>=str.length() || str.charAt(i+1)!=str.charAt(i)) {
+				String tmp = ""+str.charAt(i)+count;
+				compressedString=compressedString.append(tmp);
+				count=1;
+			}else {
+				count++;
+			}
+		}
+		return compressedString;
+	}
+
+}
